bff5a642ea7119652255bdc61253d06507bf7660
Lucas Moreira <48088328+AnalystLucas@users.noreply.github.com>
2020-03-09 15:51:22 -0300

    Update alterar_dados.php
    Criação da Função para alterar o ponto

--- Controller/alterar_dados.php
+++ Controller/alterar_dados.php
@@ -23,6 +23,7 @@ class AlterarDados{
             $retorno = ["message"=>"Não foi possivel alterar os dados !","retorno"=>false];
             $this->resultado = json_encode($retorno);
         }
+        mysqli_close($conn->conexao());
     }
     public function updateSituacao($matricula,$situacao){
 
@@ -41,7 +42,24 @@ class AlterarDados{
             $retorno = ["message"=>"Não foi possivel realizar a operação !","retorno"=>false];
             $this->resultado = json_encode($retorno);
         }
+        mysqli_close($conn->conexao());
     }
+    public function updatePonto($id, $field, $fieldTwo, $fieldThree, $fieldFour){
+        $conn = new Conex();
+
+        $sql = "UPDATE pontos SET entrada = '$field', intervalo = '$fieldTwo' , retorno = '$fieldThree', saida = '$fieldFour' WHERE id_ponto = '$id'";
+        $result = mysqli_query($conn->conexao(), $sql);
+
+        if($result == true){
+            $retorno = ["message"=>"Operação realizada com sucesso !","retorno"=>true];
+            $this->resultado = json_encode($retorno);
+        }else{
+            $retorno = ["message"=>"Não foi possivel realizar a operação !","retorno"=>false];
+            $this->resultado = json_encode($retorno);
+        }
+        mysqli_close($conn->conexao());
+    }
+
     public function resultado(){
         return $this->resultado;
     }
